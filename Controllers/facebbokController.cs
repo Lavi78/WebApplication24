0b8889526192ac5cb0bb432fc20c188c468a035d
admin <admin@CHETUIWK222>
2022-04-30 18:57:36 +0530

    Add project files.

---
+++ Controllers/facebbokController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication24.Controllers
+{
+    public class facebbokController : Controller
+    {
+        // GET: facebbok
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult menu()
+        {
+            return View();
+        }
+        public ActionResult contact_us()
+        {
+            return View();
+        }
+        public ActionResult about_us()
+        {
+            return View();
+        }
+        public ActionResult carrier()
+        {
+            return View();
+        }
+        public ActionResult settings()
+        {
+            return View();
+        }
+
+    }
+}
